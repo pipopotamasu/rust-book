ffc689ac81539187693727bf6306b3ac278111a7
pipopotamasu <ec0156hx@gmail.com>
2021-01-01 12:45:36 +0900

    rm target dir

--- adder/target/rls/debug/deps/adder-83bbecfd6798c0aa.d
+++
@@ -1,5 +0,0 @@
-/Users/yamatomurakami/repos/rust-book/adder/target/rls/debug/deps/adder-83bbecfd6798c0aa.rmeta: src/lib.rs
-
-/Users/yamatomurakami/repos/rust-book/adder/target/rls/debug/deps/adder-83bbecfd6798c0aa.d: src/lib.rs
-
-src/lib.rs:
