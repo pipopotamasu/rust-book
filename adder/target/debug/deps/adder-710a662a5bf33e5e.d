471d077fb978901e2f0080a1e63a54580b16b88b
pipopotamasu <ec0156hx@gmail.com>
2021-01-01 12:44:40 +0900

    rm target dir

--- adder/target/debug/deps/adder-710a662a5bf33e5e.d
+++
@@ -1,5 +0,0 @@
-/Users/yamatomurakami/repos/rust-book/adder/target/debug/deps/adder-710a662a5bf33e5e: src/lib.rs
-
-/Users/yamatomurakami/repos/rust-book/adder/target/debug/deps/adder-710a662a5bf33e5e.d: src/lib.rs
-
-src/lib.rs:
---
+++ adder/target/rls/debug/deps/integration-dd42caa22b94fb98.d
@@ -0,0 +1,5 @@
+/Users/yamatomurakami/repos/rust-book/adder/target/rls/debug/deps/integration-dd42caa22b94fb98.rmeta: tests/integration.rs
+
+/Users/yamatomurakami/repos/rust-book/adder/target/rls/debug/deps/integration-dd42caa22b94fb98.d: tests/integration.rs
+
+tests/integration.rs:
