0d06fc59c28497fb071fc2ffdda583db719eb385
pipopotamasu <ec0156hx@gmail.com>
2021-01-02 16:37:57 +0900

    Minigrep (#4)
    * rectangles
    
    * minigrep

---
+++ rectangles/target/rls/debug/deps/rectangles-cae016ffa9496d07.d
@@ -0,0 +1,5 @@
+/Users/yamatomurakami/repos/rust-book/rectangles/target/rls/debug/deps/rectangles-cae016ffa9496d07.rmeta: src/main.rs
+
+/Users/yamatomurakami/repos/rust-book/rectangles/target/rls/debug/deps/rectangles-cae016ffa9496d07.d: src/main.rs
+
+src/main.rs:
